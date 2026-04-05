75f1122c34ac8b2d1cf365e58528eb0d68234433
Divyeshkalariya <divyeshkalariya26@gmail.com>
2026-04-05 12:01:33 +0530

    test vercle live site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
         <Skills />
         <Projects />
         <Experience />
-        <Contact />
+        {/* <Contact /> */}
         <Footer />
       </main>
     </>
